74eb73fabc0401209ec671f1332927842febe91a
AgnesVelemi <agnes.kommunikation@gmail.com>
2026-04-21 10:03:00 +0200

    added git-code submenu links

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '', // Relative to current origin (port 80)
-  wsUrl: 'ws://13.63.37.93/ws/stomp',
-  dashboardUrl: 'http://13.63.37.93/api/dashboard'
+  apiUrl: '', // Use relative paths for API calls (proxied by Nginx)
+  wsUrl: 'wss://portfolio-frontend.aws.siample.dev/ws/stomp',
+  dashboardUrl: 'https://portfolio-backend.aws.siample.dev/api/dashboard'
 };
